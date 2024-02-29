7a486744bac026c13a51717a1e003e89ba906ba4
vickydrjt <vickydrajat90@gmail.com>
2024-02-29 21:41:47 +0800

    Add your commit

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+ import 'package:flutter_application_1/hello_world.dart';
+ import 'package:flutter/material.dart';
+
+ void main() {
+runApp(const MyApp());
+ }
+
+ class MyApp extends StatelessWidget {
+ const MyApp({Key? key}) : super(key: key);
+
+
+ @override
+ Widget build(BuildContext context) {
+ return const MaterialApp(
+ title: "Aplikasi Flutter Pertama",
+ home: ColumnWidget(),
+ );
+ }
+ }
